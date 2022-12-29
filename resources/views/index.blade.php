8af1dd6c57d82c300aabf9e03b49c46ba0765276
YY <nyeyint.dev@gmail.com>
2022-12-29 11:48:36 +0800

    Sort tailwindcss class to recommand  order

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,89 +1,146 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
 @section('content')
 <div class="container mx-auto px-4">
     {{-- Start For Popular Game Section --}}
-    <div class="popular-games ">
-        <h2 class="text-blue-500 uppercase tracking-wide font-simibold ">Popular Games</h2>
-        <div class="pupular-games text-sm grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-12 border-b border-gray-800 pb-16">
+    <div class="popular-games">
+        <h2 class="font-simibold uppercase tracking-wide text-blue-500">
+            Popular Games
+        </h2>
+        <div
+            class="pupular-games grid grid-cols-1 gap-12 border-b border-gray-800 pb-16 text-sm sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6"
+        >
             @for ($i = 0; $i < 12; $i++)
-                <div class="game mt-8">
-                    <div class="relative inline-block">
-                        <a href="#">
-                            <img class="rounded hover:opacity-75 transition ease-in-out duration-150" src="/ff7.jpg" alt="FF7 Image">
-                        </a>
-                        <div class="absolute w-16 h-16 bg-gray-800 rounded-full -bottom-5 -right-5">
-                            <div class="font-simibold text-xs flex justify-center items-center h-full">
-                                80%
-                            </div>
+            <div class="game mt-8">
+                <div class="relative inline-block">
+                    <a href="#">
+                        <img
+                            class="rounded transition duration-150 ease-in-out hover:opacity-75"
+                            src="/ff7.jpg"
+                            alt="FF7 Image"
+                        />
+                    </a>
+                    <div
+                        class="absolute -bottom-5 -right-5 h-16 w-16 rounded-full bg-gray-800"
+                    >
+                        <div
+                            class="font-simibold flex h-full items-center justify-center text-xs"
+                        >
+                            80%
                         </div>
                     </div>
-                    <a href="#" class="font-simibold block mt-8 leading-tight hover:text-gray-400">Final Fantasy 7 Remark</a>
-                    <div class="text-gray-400 mt-1">PS4</div>
                 </div>
+                <a
+                    href="#"
+                    class="font-simibold mt-8 block leading-tight hover:text-gray-400"
+                    >Final Fantasy 7 Remark</a
+                >
+                <div class="mt-1 text-gray-400">PS4</div>
+            </div>
             @endfor
         </div>
     </div>
     {{-- End of popular game section --}}
 
-    <div class="flex flex-col lg:flex-row my-10">
-        <div class="recently-viewed w-full lg:w-3/4 lg:mr-32">
-             <h2 class="text-blue-500 uppercase tracking-wide font-simibold">Recently Reviewed</h2>
-             @for ($i = 0; $i < 3; $i++)
-                <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img class="w-48 rounded hover:opacity-75 transition ease-in-out duration-150" src="/ff7.jpg" alt="FF7 Image">
-                            </a>
-                            <div class="absolute w-16 h-16 bg-gray-900 rounded-full -bottom-5 -right-5">
-                                <div class="font-simibold text-xs flex justify-center items-center h-full">
-                                    80%
-                                </div>
+    <div class="my-10 flex flex-col lg:flex-row">
+        <div class="recently-viewed w-full lg:mr-32 lg:w-3/4">
+            <h2 class="font-simibold uppercase tracking-wide text-blue-500">
+                Recently Reviewed
+            </h2>
+            @for ($i = 0; $i < 3; $i++)
+            <div class="recently-reviewed-container mt-8 space-y-12">
+                <div
+                    class="game flex rounded-lg bg-gray-800 px-6 py-6 shadow-md"
+                >
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img
+                                class="w-48 rounded transition duration-150 ease-in-out hover:opacity-75"
+                                src="/ff7.jpg"
+                                alt="FF7 Image"
+                            />
+                        </a>
+                        <div
+                            class="absolute -bottom-5 -right-5 h-16 w-16 rounded-full bg-gray-900"
+                        >
+                            <div
+                                class="font-simibold flex h-full items-center justify-center text-xs"
+                            >
+                                80%
                             </div>
                         </div>
-                        <div class="ml-12">
-                            <a href="#" class="font-simibold text-lg block mt-4 leading-tight hover:text-gray-400">Final Fantasy 7 Remark</a>
-                            <div class="text-gray-400 mt-1">Playstatioun 4</div>
-                            <p class="mt-6 text-gray-400 hidden lg:block">
-                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti ea odit rem amet aspernatur earum eveniet. Asperiores quo laboriosam, magnam vero praesentium at error modi. Sint cupiditate accusantium magni doloremque.
-                            </p>
+                    </div>
+                    <div class="ml-12">
+                        <a
+                            href="#"
+                            class="font-simibold mt-4 block text-lg leading-tight hover:text-gray-400"
+                            >Final Fantasy 7 Remark</a
+                        >
+                        <div class="mt-1 text-gray-400">Playstatioun 4</div>
+                        <p class="mt-6 hidden text-gray-400 lg:block">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Deleniti ea odit rem amet aspernatur earum
+                            eveniet. Asperiores quo laboriosam, magnam vero
+                            praesentium at error modi. Sint cupiditate
+                            accusantium magni doloremque.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+        <div class="most-anticipated mt-12 lg:mt-0 lg:w-1/4">
+            <h2 class="font-simibold uppercase tracking-wide text-blue-500">
+                Most Anticipated
+            </h2>
+            <div class="most-anticipated-container mt-8 space-y-10">
+                @for ($i = 0; $i < 4; $i++)
+                <div class="game flex">
+                    <a href="#">
+                        <img
+                            class="w-16 rounded transition duration-150 ease-in-out hover:opacity-75"
+                            src="/ff7.jpg"
+                            alt="FF7 Image"
+                        />
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">
+                            Cyberpunk 2017</a
+                        >
+                        <div class="mt-1 text-sm text-gray-400">
+                            Sept 16, 2017
                         </div>
                     </div>
                 </div>
-             @endfor
-        </div>
-        <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
-             <h2 class="text-blue-500 uppercase tracking-wide font-simibold">Most Anticipated</h2>
-             <div class="most-anticipated-container space-y-10 mt-8">
+                @endfor
+            </div>
+            <h2
+                class="font-simibold mt-8 uppercase tracking-wide text-blue-500"
+            >
+                Cooming Soon
+            </h2>
+            <div class="most-anticipated-container mt-8 space-y-10">
                 @for ($i = 0; $i < 4; $i++)
-                    <div class="game flex">
-                        <a href="#">
-                            <img class="rounded w-16 hover:opacity-75 transition ease-in-out duration-150" src="/ff7.jpg" alt="FF7 Image">
-                        </a>
-                        <div class="ml-4">
-                            <a href="#" class="hover:text-gray-300"> Cyberpunk 2017</a>
-                            <div class="text-gray-400 text-sm mt-1"> Sept 16, 2017</div>
+                <div class="game flex">
+                    <a href="#">
+                        <img
+                            class="w-16 rounded transition duration-150 ease-in-out hover:opacity-75"
+                            src="/ff7.jpg"
+                            alt="FF7 Image"
+                        />
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">
+                            Cyberpunk 2017</a
+                        >
+                        <div class="mt-1 text-sm text-gray-400">
+                            Sept 16, 2017
                         </div>
                     </div>
+                </div>
                 @endfor
-             </div>
-             <h2 class="text-blue-500 uppercase tracking-wide font-simibold mt-8">Cooming Soon</h2>
-             <div class="most-anticipated-container space-y-10 mt-8">
-                @for ($i = 0; $i < 4; $i++)
-                    <div class="game flex">
-                        <a href="#">
-                            <img class="rounded w-16 hover:opacity-75 transition ease-in-out duration-150" src="/ff7.jpg" alt="FF7 Image">
-                        </a>
-                        <div class="ml-4">
-                            <a href="#" class="hover:text-gray-300"> Cyberpunk 2017</a>
-                            <div class="text-gray-400 text-sm mt-1"> Sept 16, 2017</div>
-                        </div>
-                    </div>
-                @endfor
-             </div>
+            </div>
         </div>
     </div>
-
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,84 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
- <meta charset="UTF-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Lara Video Games</title>
-  @vite('resources/css/app.css')
-</head>
-<body class="bg-gray-900 text-white">
- <header class="border-b border-gray-800">
-    <nav class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
-       <div class="flex flex-col lg:flex-row items-center">
-         <a href="/">
-          <img class="flex-none w-48 " src="/logo.svg" alt="Lara Video Games Logo">
-         </a>
-         <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
-            <li><a href="#" class="hover:text-gray-400">Games</a></li>
-            <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
-            <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
-         </ul>
-       </div>
-       <div class="flex items-center mt-6 lg:mt-0">
-         <div class="relative">
-            <input type="text" class="bg-gray-800 text-sm rounded-full px-3 py-1 focus:outline-none focus:shadow-outline w-64 pl-8" placeholder="Search...">
-            <div class="absolute top-0 flex items-center h-full ml-2">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" class=" text-gray-400 w-4" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607z"/></svg>
-            </div>
-         </div>
-         <div class="ml-6">
-          <a href="#">
-           <img class="rounded-full w-8" src="/avatar.jpg" alt="avater">
-          </a>
-         </div>
-       </div>
-    </nav>
- </header>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Lara Video Games</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-gray-900 text-white">
+        <header class="border-b border-gray-800">
+            <nav
+                class="container mx-auto flex flex-col items-center justify-between px-4 py-6 lg:flex-row"
+            >
+                <div class="flex flex-col items-center lg:flex-row">
+                    <a href="/">
+                        <img
+                            class="w-48 flex-none"
+                            src="/logo.svg"
+                            alt="Lara Video Games Logo"
+                        />
+                    </a>
+                    <ul class="ml-0 mt-6 flex space-x-8 lg:ml-16 lg:mt-0">
+                        <li>
+                            <a href="#" class="hover:text-gray-400">Games</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-gray-400">Reviews</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:text-gray-400"
+                                >Coming Soon</a
+                            >
+                        </li>
+                    </ul>
+                </div>
+                <div class="mt-6 flex items-center lg:mt-0">
+                    <div class="relative">
+                        <input
+                            type="text"
+                            class="focus:shadow-outline w-64 rounded-full bg-gray-800 px-3 py-1 pl-8 text-sm focus:outline-none"
+                            placeholder="Search..."
+                        />
+                        <div
+                            class="absolute top-0 ml-2 flex h-full items-center"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="1.5"
+                                class="w-4 text-gray-400"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607z"
+                                />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="ml-6">
+                        <a href="#">
+                            <img
+                                class="w-8 rounded-full"
+                                src="/avatar.jpg"
+                                alt="avater"
+                            />
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </header>
 
- <main class="py-8 ">
-   @yield('content')
- </main>
- <footer class="border-t border-gray-800">
-  <div class="container mx-auto px-4 py-6">
-    Powered By <a href="#" class="underline hover:text-gray-400"> IGDB API</a>
-  </div>
- </footer>
-</body>
+        <main class="py-8">@yield('content')</main>
+        <footer class="border-t border-gray-800">
+            <div class="container mx-auto px-4 py-6">
+                Powered By
+                <a href="#" class="underline hover:text-gray-400"> IGDB API</a>
+            </div>
+        </footer>
+    </body>
 </html>
